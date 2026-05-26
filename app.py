2dfa3a97e1f2993853b1485a92af7657b78e037f
thuanluong31241027504-sketch <thuanluong.31241027504@st.ueh.edu.vn>
2026-05-26 16:48:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="Emoji Classifier", layout="centered")
 
 # ==================== THAM SỐ DỊCH CHUYỂN ====================
 CANVAS_SHIFT = 200     # Dịch canvas (px) - số dương sang phải, âm sang trái
-BUTTON_SHIFT = 0      # Dịch nút (px) - số dương sang phải, âm sang trái
+BUTTON_SHIFT = 100      # Dịch nút (px) - số dương sang phải, âm sang trái
 
 st.markdown(f"""
 <style>
